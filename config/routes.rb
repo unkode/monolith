1eee241c6aa2b29fb09df93fb920eb1df8c0a379
unkode <monolith.contact@gmail.com>
2017-01-24 05:13:12 -0500

    backup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
 
-  # devise_for :users
+  root to: 'dashboard#index'
+
+  #devise_for :users
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
-  
-
+ 
   resources :users
-
-  
 
 end
